fe46acf47e39814cf267a7b59259fc9fa6216888
lNikol <itstudentlll267@gmail.com>
2021-01-12 17:38:11 +0300

    NEW BlackJackOOP

---
+++ card.cpp
@@ -0,0 +1,10 @@
+#include "card.h"
+Card::Card() {
+	m_rank = CARD_2,
+	m_suit =CARD_DIAMONDS,
+};
+
+Card::Card(CardRank f_rank,CardSuits f_suit) {
+	m_rank = f_rank,
+	m_suit = f_suit,
+};
---
+++ deck.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include "card.h"
+#include <vector>
+class Deck :Card {
+	std::vector<int> Deck(52);
+
+};
